dfcc5600e353f592368331936446a1c136a8daa7
Bov888 <skenderbay@gmx.com>
2026-04-21 13:51:33 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
-const CACHE_NAME = 'scanner-v1';
-
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
+self.addEventListener('install', () => {
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', (event) => {
-    // Дозволяємо браузеру просто завантажувати файли
-    event.respondWith(fetch(event.request));
+  // Порожній обробник, щоб Chrome вважав PWA валідним
 });
