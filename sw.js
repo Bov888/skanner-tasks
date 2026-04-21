49b99687b1f1ec8136dba45e0da328db80719c18
Bov888 <skenderbay@gmx.com>
2026-04-21 14:04:24 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,12 @@
-self.addEventListener('install', () => {
+self.addEventListener('install', (event) => {
+  console.log('SW installed');
   self.skipWaiting();
 });
 
+self.addEventListener('activate', (event) => {
+  console.log('SW activated');
+});
+
 self.addEventListener('fetch', (event) => {
-  // Порожній обробник, щоб Chrome вважав PWA валідним
+  // Порожній обробник обов'язковий для PWA
 });
